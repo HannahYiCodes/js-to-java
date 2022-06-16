6097a899482ba20ef89aeec7f4798a1d1fc7ff28
HannahYiCodes <106121229+HannahYiCodes@users.noreply.github.com>
2022-06-15 21:25:37 -0400

    Update findtheindex.js

--- findtheindex.js
+++ findtheindex.js
@@ -1,3 +1,6 @@
+// The first of many excersizes
+// SEPERATE ALL OF THEM
+
 // Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
 
 // examples:
